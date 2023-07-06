a1aab78a4b91041cb76ca50149937f622517d92c
Quang Dang <quang.dang@codegym.vn>
2023-07-06 09:58:00 +0700

    bien va toan tu

---
+++ JS/ifelse/ifelse.js
@@ -0,0 +1,19 @@
+/**
+var a = 5;
+if(a++ > 5){
+    console.log("A > 5");
+}else{
+    console.log("A không lớn hơn 5");
+}
+console.log(a);
+ */
+
+
+var a = 5;
+// false || true=>true
+var z = 90;
+if(a === "5" || (z=0)){
+    console.log("A = 5");
+}else{
+    console.log("A không 5");
+}
